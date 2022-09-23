937e1d1d64b71fc9a7e7091046084f19381514e8
Edward Kojo Boateng Ntow <kojoboateng514@gmail.com>
2022-09-23 13:35:09 +0200

    Leap Year Test

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace leapchkYearTest
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int chkYear;
+            Console.WriteLine("");
+            Console.WriteLine("Check whether a given year is leap year or not: ");
+            Console.WriteLine("................................. ");
+            Console.WriteLine("");
+            Console.WriteLine("Enter year: ");
+            chkYear = Convert.ToInt32(Console.ReadLine());
+
+            if (chkYear % 400 == 0)
+            {
+                Console.Write(chkYear + " is a leap Year.");
+            }
+            else if (chkYear % 100 == 0)
+            {
+                Console.Write(chkYear + " is not a leap Year.");
+            }
+            else if (chkYear % 4 == 0)
+            {
+                Console.Write(chkYear + " is a leap Year.");
+            }
+            else
+            {
+                Console.Write(chkYear + " is not a leap Year.");
+            }
+
+        }
+    }
+}
